1d4461092f5aef46a8fe0da8bf09d47dc2dbcb93
blackstream-x <rainer@blackstream.de>
2021-07-08 15:47:17 +0200

    Completed the VBR-header-fixing script

--- fix_vbr_lengths.py
+++ fix_vbr_lengths.py
@@ -10,12 +10,17 @@ Copy audio files with fixed VBR header into a new directory
 
 
 import argparse
-import logging
-# import os
+import hashlib
+import mimetypes
+import os
 import pathlib
-# import re
-# import subprocess
+import subprocess
 import sys
+
+
+# local module
+
+import dialog
 
 
 #
@@ -23,8 +28,17 @@ import sys
 #
 
 
-RC_OK = 0
-RC_ERROR = 1
+INTERROGATOR = dialog.Interrogator()
+LOGGER = INTERROGATOR.logger
+
+# Default output directory suffix
+DOD_SUFFIX = '.vbr_headers_fixed'
+
+# Results viasualization: check mark or ballot x
+VISUALIZATION = {True: '\u2713', False: '\u2717'}
+
+RETURNCODE_OK = 0
+RETURNCODE_ERROR = 1
 
 
 #
@@ -32,77 +46,179 @@ RC_ERROR = 1
 #
 
 
+def file_hexdigest(file_path, algorithm=hashlib.sha256):
+    """Return the hexdigest of a file"""
+    return algorithm(file_path.read_bytes()).hexdigest()
+
+
 def fix_file_vbr_length(file_path, target_directory_path):
     """Write a new file with fixed VBR header
-    to target_directory_path using ffmpeg
+    to target_directory_path using ffmpeg,
+    according to <https://askubuntu.com/a/1000020>
     """
-    # TODO: https://askubuntu.com/a/1000020
-    # subprocess:
-    # 'ffmpeg', '-i', str(file_path), '-acodec', 'copy',
-    # str(target_directory_path / file_path.name)
+    LOGGER.heading(
+        'Processing %r…', file_path.name, style=dialog.BoxFormatter.double)
+    output_file_path = target_directory_path / file_path.name
+    try:
+        completed_process = subprocess.run(
+            ['ffmpeg', '-i', str(file_path), '-acodec', 'copy',
+             str(output_file_path)],
+            capture_output=True,
+            check=True)
+    except subprocess.CalledProcessError as error:
+        if error.stdout:
+            LOGGER.heading('Standard output')
+            LOGGER.info(error.stdout.decode())
+        #
+        if error.stderr:
+            LOGGER.heading('Standard error')
+            LOGGER.error(error.stderr.decode())
+        #
+        return False
     #
-    # Return False on exceptions or errors
+    if completed_process.stdout:
+        LOGGER.heading('Standard output', level=dialog.logging.DEBUG)
+        LOGGER.debug(completed_process.stdout.decode())
+    #
+    if completed_process.stderr:
+        LOGGER.heading('Standard error', level=dialog.logging.DEBUG)
+        LOGGER.debug(completed_process.stderr.decode())
+    #
+    original_digest = file_hexdigest(file_path)
+    fixed_file_digest = file_hexdigest(output_file_path)
+    if fixed_file_digest == original_digest:
+        LOGGER.heading('File unchanged')
+        LOGGER.info(
+            'File_size: %s bytes', os.stat(output_file_path).st_size)
+        LOGGER.debug(
+            '… checksum: %s', fixed_file_digest)
+    else:
+        LOGGER.heading('File changed')
+        LOGGER.info(
+            'Old file size: %s bytes', os.stat(file_path).st_size)
+        LOGGER.debug(
+            '… checksum: %s', original_digest)
+        LOGGER.info(
+            'New file_size: %s bytes', os.stat(output_file_path).st_size)
+        LOGGER.debug(
+            '… checksum: %s', fixed_file_digest)
+    #
     return True
+
+
+def fix_all_files(input_directory, output_directory):
+    """Write fixed files from input_directory to output_directory"""
+    LOGGER.info(
+        'Fixing VBR headers of files in \n    %s\n'
+        'writing fixed files to\n    %s',
+        input_directory,
+        output_directory)
+    access_mode = os.stat(input_directory).st_mode & 0o777
+    try:
+        output_directory.mkdir(access_mode, parents=True)
+    except FileExistsError:
+        LOGGER.critical(
+            'Output directory %r already exists!', str(output_directory))
+        return RETURNCODE_ERROR
+    #
+    processed_files = []
+    for file_path in sorted(input_directory.glob('*')):
+        if not file_path.is_file():
+            LOGGER.debug(
+                'Skipped %r: not a regular file', file_path.name)
+            continue
+        #
+        mime_type = mimetypes.guess_type(file_path)[0]
+        if not mime_type or not mime_type.startswith('audio/'):
+            LOGGER.debug(
+                'Skipped file %r: no audio mime type', file_path.name)
+            continue
+        #
+        processed_files.append(
+            (file_path, fix_file_vbr_length(file_path, output_directory)))
+    #
+    errors = 0
+    LOGGER.heading('Results', style=dialog.BoxFormatter.heavy)
+    for (file_path, fix_result) in processed_files:
+        LOGGER.info('%s %s', VISUALIZATION[fix_result], file_path.name)
+        if not fix_result:
+            errors += 1
+        #
+    #
+    LOGGER.separator()
+    LOGGER.info(
+        '%s files processed, %s errors', len(processed_files), errors)
+    if errors:
+        return RETURNCODE_ERROR
+    #
+    return RETURNCODE_OK
 
 
 def __get_arguments():
     """Parse command line arguments"""
     argument_parser = argparse.ArgumentParser(
-        description='Get and print data from a musicbrainz release')
-    argument_parser.set_defaults(loglevel=logging.INFO)
+        description='Fix VBR headers of the files in the specified directory')
+    argument_parser.set_defaults(
+        loglevel=dialog.logging.INFO,
+        input_directory=pathlib.Path.cwd())
     argument_parser.add_argument(
         '-v', '--verbose',
         action='store_const',
-        const=logging.DEBUG,
+        const=dialog.logging.DEBUG,
         dest='loglevel',
         help='Output all messages including debug level')
     argument_parser.add_argument(
         '-q', '--quiet',
         action='store_const',
-        const=logging.WARNING,
+        const=dialog.logging.WARNING,
         dest='loglevel',
         help='Limit message output to warnings and errors')
     argument_parser.add_argument(
-        '-d', '--directory',
+        '-d', '--input-directory',
         type=pathlib.Path,
-        help='A directory with a release'
+        help='A directory with audio files'
         ' (defaults to the current directory, in this case:'
-        '%(default)s)')
+        ' %(default)s)')
     argument_parser.add_argument(
-        'dummy',
-        nargs=argparse.REMAINDER)
+        '-o', '--output-directory',
+        type=pathlib.Path,
+        help='The target directory for the fixed files'
+        ' (must not exist yet, will be created!'
+        ' Defaults to the input directory with %r attached' % DOD_SUFFIX)
     return argument_parser.parse_args()
 
 
 def main(arguments=None):
     """Main script function"""
-    selected_directory = None
-    try:
-        loglevel = arguments.loglevel
-        selected_directory = arguments.directory
-    except AttributeError:
-        loglevel = logging.WARNING
+    LOGGER.configure(level=arguments.loglevel)
+    input_directory = arguments.input_directory.resolve()
+    if input_directory.exists():
+        if not input_directory.is_dir():
+            LOGGER.info(
+                '%r is a file. Swithing to its parent directory instead…',
+                str(input_directory))
+            input_directory = input_directory.parent
+        #
+    else:
+        LOGGER.exit_with_error(
+            'Input directory %r does not exist!', str(input_directory))
     #
-    if selected_directory and not selected_directory.is_dir():
-        selected_directory = selected_directory.parent
+    if arguments.output_directory:
+        if arguments.output_directory.is_absolute():
+            output_directory = arguments.output_directory
+        else:
+            output_directory = pathlib.Path(
+                os.path.normpath(
+                    input_directory / arguments.output_directory))
+        #
+    else:
+        output_directory = input_directory.with_suffix(DOD_SUFFIX)
     #
-    logging.basicConfig(
-        format='%(levelname)-8s\u2551 %(funcName)s → %(message)s',
-        level=loglevel)
-
-    return RC_OK
+    return fix_all_files(input_directory, output_directory)
 
 
 if __name__ == '__main__':
-    # =========================================================================
-    # Workaround for unexpected behavior when called
-    # as a Nautilus script in combination with argparse
-    # =========================================================================
-    try:
-        sys.exit(main(__get_arguments()))
-    except Exception:
-        sys.exit(main())
-    #
+    sys.exit(main(__get_arguments()))
 
 
 # vim: fileencoding=utf-8 ts=4 sts=4 sw=4 autoindent expandtab syntax=python:
